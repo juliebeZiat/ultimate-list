19545c8429123869063b4a810fdc76e90c2af4de
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-04 20:01:33 +0200

    [Integration] list page style and app style

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,7 @@
 // == Import style
 import './liste.scss';
 import {
-  Button, Grid, Icon, Item, Label, Menu,
+  Button, Grid, Icon, Label, Menu, Image, Header,
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
@@ -24,7 +24,7 @@ const List = () => (
 
         <Grid.Row>
           <Grid.Column computer={2} only="computer tablet">
-            <Menu size="massive" secondary compact vertical icon>
+            <Menu inverted size="massive" secondary compact vertical icon>
               <Menu.Item active>
                 <Icon name="gamepad" />
               </Menu.Item>
@@ -44,13 +44,13 @@ const List = () => (
             </div>
 
             <div className="list-header-toggle">
-              <Menu text>
+              <Menu text inverted>
                 <Menu.Item
                   name="Ma liste"
                   active
                 />
                 <Menu.Item>
-                  <Icon name="toggle off" className="list-toggle-icon" />
+                  <Icon name="toggle off" className="list-header-toggle-icon" />
                 </Menu.Item>
                 <Menu.Item
                   name="Recommandations"
@@ -60,10 +60,10 @@ const List = () => (
 
             <div className="list-header-progress">
               <div className="list-header-progress-status">
-                <Button circular>Tous</Button>
-                <Button circular>À jouer</Button>
-                <Button circular>En cours</Button>
-                <Button circular>Fini</Button>
+                <button type="button" className="list-header-progress-status-button-active">Tous</button>
+                <button type="button" className="list-header-progress-status-button">À jouer</button>
+                <button type="button" className="list-header-progress-status-button">En cours</button>
+                <button type="button" className="list-header-progress-status-button">Fini</button>
               </div>
             </div>
 
@@ -72,26 +72,82 @@ const List = () => (
       </Grid>
     </div>
 
+    <div className="list-add">
+      <Button circular className="list-add-button">
+        <Link className="list-add-button-link" to="/jeuxvideo/ajouter">
+          + Ajouter un item
+        </Link>
+      </Button>
+    </div>
+
     <div className="list-items">
-      <div className="list-add">
-        <Button circular>
-          <Link to="/jeuxvideo/ajouter">
-            + Ajouter un item
-          </Link>
-        </Button>
-      </div>
       <div className="list-items-date">Mars 2022</div>
-      <Item.Group unstackable>
-        <Item className="list-items-item">
-          <Item.Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
-          <Item.Content className="list-items-item-content">
-            <Item.Header>Stardew Valley</Item.Header>
-            <Item.Extra>
-              <Label size="mini" className="list-items-item-status">En cours</Label>
-            </Item.Extra>
-          </Item.Content>
-        </Item>
-      </Item.Group>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} className="list-items-item-content">
+        <Grid.Row>
+          <Grid.Column computer={3}>
+            <Image size="tiny" src="https://i.picsum.photos/id/361/100/100.jpg?hmac=XkcZtgG_QqVwd_xSW0st9Gfwg2srT7czclvuW_lGtwA" />
+          </Grid.Column>
+          <Grid.Column className="list-items-item-content" computer={13}>
+            <Header as="h4" className="list-items-item-content-title">Zelda</Header>
+            <Label size="mini" className="list-items-item-status">En cours</Label>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   </div>
 );
