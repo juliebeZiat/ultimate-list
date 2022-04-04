1e34195656614705de86e5e5f68b141da6a1ab60
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-04 09:58:03 +0200

    [Integration] change title and favicon

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1>Ultimate List</h1>
   </div>
 );
 
