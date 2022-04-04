ca50305dffd11c2732fe6e22355a3f7ad996aa68
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-04 14:24:09 +0200

    [Integration] list page static (no style)

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,16 +1,60 @@
 // == Import style
 import './liste.scss';
-import { Button } from 'semantic-ui-react';
+import {
+  Button, Icon, Item, Label, Menu, Radio,
+} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const List = () => (
-  <div>
-    <Button>
-      <Link to="/jeuxvideo/ajouter">
-        Ajouter un item
-      </Link>
-    </Button>
-    <div>La Liste youpi</div>
+  <div className="header">
+    <div className="list-menu">
+      <Menu icon vertical>
+        <Menu.Item>
+          <Icon name="gamepad" />
+        </Menu.Item>
+        <Menu.Item>
+          <Icon name="video play" />
+        </Menu.Item>
+      </Menu>
+    </div>
+
+    <div className="list-header">
+      <Icon name="gamepad" className="list-header-icon" />
+      <div className="list-header-title">Jeux vidéo</div>
+      <Label circular className="list-header-label">15</Label>
+    </div>
+
+    <div className="list-toggle">
+      <div className="list-toggle-list">Ma liste</div>
+      <Radio toggle className="list-toggle-icon" />
+      <div className="list-toggle-reco">Recommandations</div>
+    </div>
+
+    <div className="list-progress">
+      <Button>Tous</Button>
+      <Button>À jouer</Button>
+      <Button>En cours</Button>
+      <Button>Fini</Button>
+      <Button>
+        <Link to="/jeuxvideo/ajouter">
+          Ajouter un item
+        </Link>
+      </Button>
+    </div>
+    <div className="list-items">
+      <div className="list-items-date">Mars 2022</div>
+      <Item.Group>
+        <Item className="list-items-item">
+          <Item.Image />
+          <Item.Content>
+            <Item.Header className="list-items-item-image">Stardew Valley</Item.Header>
+            <Item.Extra>
+              <Label className="list-items-item-status">En cours</Label>
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+    </div>
   </div>
 );
 
