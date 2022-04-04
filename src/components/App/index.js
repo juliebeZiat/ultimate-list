a3791010d7211ab4215b049ab19a45e58d1ab924
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-04 10:21:25 +0200

    [Build] react-router-dom install

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,14 @@
-// == Import
+// == Import style
 import './styles.css';
+import 'semantic-ui-css/semantic.min.css';
 
 // == Composant
 const App = () => (
   <div className="app">
     <h1>Ultimate List</h1>
+    <div>header</div>
+    <div>main</div>
+    <div>footer</div>
   </div>
 );
 
