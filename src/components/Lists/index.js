82158473c11a5f8efc13a130a9e2e8a2f3edb55f
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-05 15:07:37 +0200

    [Refactor] composant Lists

---
+++ src/components/Lists/index.js
@@ -0,0 +1,30 @@
+// == Import style
+import './list.scss';
+import './add.scss';
+import { Icon } from 'semantic-ui-react';
+
+// == Import Component
+import { Link } from 'react-router-dom';
+
+const Lists = () => (
+  <div className="list-header">
+    <div className="list-header-menu">
+      <Link to="/jeuxvideo/liste">
+        <div className="list-header-menu-mode active">
+          <Icon size="big" name="gamepad" />
+        </div>
+      </Link>
+      <div className="list-header-menu-mode"><Icon size="big" name="video play" /></div>
+    </div>
+
+    <div className="list-header-title">
+      <div className="list-header-title-mode">
+        Jeux vidéo
+        <span className="list-header-title-label">15</span>
+      </div>
+    </div>
+  </div>
+);
+
+// == Export
+export default Lists;
