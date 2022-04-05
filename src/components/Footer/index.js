512e6bf517ebc62020d8b361a08194c47cdc16cd
VivienGrz <grzelka_vivien@hotmail.fr>
2022-04-05 11:12:25 +0200

    [Build] integration footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,36 @@
 import './footer.scss';
 
 const Footer = () => (
-  <div>Ici c'est le footer</div>
+  <div className="main-footer">
+    <div className="container">
+      <div className="row">
+        {/* Column1 */}
+        <div className="col">
+          <h4>Qui sommes-nous ?</h4>
+        </div>
+        {/* Column2 */}
+        <div className="col">
+          <h4>Contactez-nous</h4>
+        </div>
+        {/* Column3 */}
+        <div className="col">
+          <h3>Ultimate List</h3>
+          <ul className="list-unstyled">
+            <li>&copy; 2022 | Ultimate List</li>
+          </ul>
+        </div>
+        {/* Column4 */}
+        <div className="col">
+          <h4>Conditions générales d'utilisation</h4>
+        </div>
+        {/* Column5 */}
+        <div className="col">
+          <h4>Mentions Légales</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+
 );
 
 export default Footer;
