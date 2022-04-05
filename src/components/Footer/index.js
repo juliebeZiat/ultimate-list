ad61287281e18d08376cf6bb54f091eeee8421f9
VivienGrz <grzelka_vivien@hotmail.fr>
2022-04-05 12:15:36 +0200

    [Build] integration footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,34 +2,30 @@
 import './footer.scss';
 
 const Footer = () => (
-  <div className="main-footer">
-    <div className="container">
-      <div className="row">
-        {/* Column1 */}
-        <div className="col">
-          <h4>Qui sommes-nous ?</h4>
-        </div>
-        {/* Column2 */}
-        <div className="col">
-          <h4>Contactez-nous</h4>
-        </div>
-        {/* Column3 */}
-        <div className="col">
-          <h3>Ultimate List</h3>
-          <ul className="list-unstyled">
-            <li>&copy; 2022 | Ultimate List</li>
-          </ul>
-        </div>
-        {/* Column4 */}
-        <div className="col">
-          <h4>Conditions générales d'utilisation</h4>
-        </div>
-        {/* Column5 */}
-        <div className="col">
-          <h4>Mentions Légales</h4>
-        </div>
+  <div className="ui container">
+    <div className="ui grid">
+      {/* Column1 */}
+      <div className="sixteen wide mobile eight wide tablet three wide computer column">
+        <h6 className="ui small header">Qui sommes-nous ?</h6>
+      </div>
+      {/* Column2 */}
+      <div className="sixteen wide mobile eight wide tablet three wide computer column">
+        <h6 className="ui small header">Contactez-nous</h6>
+      </div>
+      {/* Column3 */}
+      <div className="sixteen wide mobile eight wide tablet three wide computer column">
+        <h3>Ultimate List</h3>
+      </div>
+      {/* Column4 */}
+      <div className="sixteen wide mobile eight wide tablet three wide computer column">
+        <h6 className="ui small header">Conditions générales d'utilisation</h6>
+      </div>
+      {/* Column5 */}
+      <div className="sixteen wide mobile eight wide tablet three wide computer column">
+        <h6 className="ui small header">Mentions Légales</h6>
       </div>
     </div>
+    <p className="ui center aligned tiny grey header">&copy; 2022 | Ultimate List</p>
   </div>
 
 );
