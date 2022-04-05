d970ca312ceb4965dd29e058e46d80e4a0e62156
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-05 13:32:26 +0200

    [Integration] static connexion page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,33 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 // == Import style
+import { Form } from 'semantic-ui-react';
 import './login.scss';
 
 const Login = () => (
-  <div>Ici ça va se connecter chacal</div>
+  <div className="connexion">
+
+    <div className="connexion-header">
+      <div className="connexion-header-title">Bienvenue sur <span className="connexion-header-title-bold">Ultimate List</span></div>
+      <div className="connexion-header-buttons">
+        <button type="button" className="connexion-header-buttons-login">Connexion</button>
+        <button type="button" className="connexion-header-buttons-signup">Inscription</button>
+      </div>
+    </div>
+
+    <div className="connexion-form">
+      <Form>
+        <Form.Field classname="connexion-form-field">
+          <label htmlFor="">Username ou e-mail</label>
+          <input type="text" placeholder="hello@world.io" />
+        </Form.Field>
+        <Form.Field classname="connexion-form-field">
+          <label htmlFor="">Username ou e-mail</label>
+          <input type="password" />
+        </Form.Field>
+        <button type="submit">Let's go!</button>
+      </Form>
+    </div>
+  </div>
 );
 
 export default Login;
