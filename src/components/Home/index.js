39837e3a08ec4a8730255a3dabb04d4d9bcefd7a
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-06 13:55:35 +0200

    [Integration] small changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,14 @@
 // == Import style
 import './home.scss';
 import HomeLogin from './HomeLogin';
-
-// A importer une fois la condition d'affichage selon connexion établie :
-// import HomeLogout from './HomeLogout';
+import HomeLogout from './HomeLogout';
 
 const Home = () => (
   <div className="home">
     <h1 className="home-title">Ultimate List</h1>
     <h2 className="home-subtitle">Découvrez. Partagez. Créez.</h2>
     <p className="home-tagline">Créez d’incroyables todo-list pour sauvegarder et organiser tous vos souvenirs et recommandations de vos amis</p>
-    {/* Lorsque l'on est deconnecté, on voit <HomeLogout /> */}
+    <HomeLogout />
     <HomeLogin />
   </div>
 
