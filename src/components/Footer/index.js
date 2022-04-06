775b4ba23c8e1466c4b41428e5f43af863bf7db2
VivienGrz <grzelka_vivien@hotmail.fr>
2022-04-06 11:28:07 +0200

    [Integration] Header and Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,31 +1,42 @@
 // == Import style
 import './footer.scss';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <div className="footer">
     <div className="footer-columns">
       {/* Column1 */}
       <div className="footer-column1">
-        <h6 className="title1">Qui sommes-nous ?</h6>
+        <Link to="/">
+          <h6 className="title1">Qui sommes-nous ?</h6>
+        </Link>
       </div>
       {/* Column2 */}
       <div className="footer-column2">
-        <h6 className="title2">Contactez-nous</h6>
+        <Link to="/">
+          <h6 className="title2">Contactez-nous</h6>
+        </Link>
       </div>
       {/* Column3 */}
       <div className="footer-column3">
-        <h3 className="title3">Ultimate List</h3>
+        <Link to="/">
+          <h3 className="title3">Ultimate List</h3>
+        </Link>
+        <p className="copyright">&copy; 2022 | Ultimate List</p>
       </div>
       {/* Column4 */}
       <div className="footer-column4">
-        <h6 className="title4">Conditions générales d'utilisation</h6>
+        <Link to="/">
+          <h6 className="title4">Conditions générales d'utilisation</h6>
+        </Link>
       </div>
       {/* Column5 */}
       <div className="footer-column5">
-        <h6 className="title5">Mentions Légales</h6>
+        <Link to="/">
+          <h6 className="title5">Mentions Légales</h6>
+        </Link>
       </div>
     </div>
-    <p className="copyright">&copy; 2022 | Ultimate List</p>
   </div>
 
 );
