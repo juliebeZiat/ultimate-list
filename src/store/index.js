25d7ad79c0c415b778a9a5ddd86154a1ebc1c7f2
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-08 14:10:45 +0200

    [Build] connect API user list

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from 'src/reducers';
 import itemsApiMiddleware from 'src/middlewares/itemsApiMiddleware';
+import userItemsApiMiddleware from 'src/middlewares/userItemsApiMiddleware';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
   applyMiddleware(
     itemsApiMiddleware,
+    userItemsApiMiddleware,
   ),
 );
 
