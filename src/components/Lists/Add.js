bf78c160605d43b6ca1596c43e02446e576c241b
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-08 15:56:13 +0200

    [Build] function created to filter items by mode, and used in Add Page to dynamize card's display

--- src/components/Lists/Add.js
+++ src/components/Lists/Add.js
@@ -1,11 +1,16 @@
 // == Import
 import { useSelector } from 'react-redux';
+// import { useParams } from 'react-router-dom';
+import { findItemsByMode } from 'src/functions/items';
 
 // == Import Component
 import Lists from '.';
 
 const Add = () => {
   const items = useSelector((state) => state.items.list);
+  // to use when items gets a "slug" property
+  // const { slug } = useParams();
+  const itemsFiltered = findItemsByMode(items, 'videoGames');
 
   return (
     <>
@@ -24,7 +29,7 @@ const Add = () => {
         <div className="add-reco">
           <h2 className="add-modSubtitle">Nos recommandations pour toi</h2>
           <ul className="add-reco-cards">
-            {items.map((item) => (
+            {itemsFiltered.map((item) => (
               <li className="add-reco-cards-card" key={item.id}>
                 <img className="card-image" src={item.image} alt="" />
                 <div className="card-content">
@@ -32,7 +37,7 @@ const Add = () => {
                   <p className="card-description">{item.developer}</p>
                   <div className="card-tags">
                     {item.tags.map((tag) => (
-                      <span className="card-tag" key={tag.id}>{tag.id}</span>
+                      <span className="card-tag" key={tag.id}>{tag.name}</span>
                     ))}
                   </div>
                 </div>
