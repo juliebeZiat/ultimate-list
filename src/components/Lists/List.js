5c83f89a00858cc7aaa90bd50f6a981cffd8882f
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-08 16:29:40 +0200

    [Build] filtered items on page list with the itemsFiltered function

--- src/components/Lists/List.js
+++ src/components/Lists/List.js
@@ -1,5 +1,6 @@
 // == Import
 import { useSelector } from 'react-redux';
+import { findItemsByMode } from 'src/functions/items';
 
 // == Import style
 import Toggle from 'src/assets/icons/toggle-on.svg';
@@ -11,6 +12,9 @@ import Lists from '.';
 
 const List = () => {
   const userItems = useSelector((state) => state.userItems.user_list);
+
+  // Filter items according to the slug videoGames
+  const itemsFiltered = findItemsByMode(userItems, 'videoGames');
 
   const convertDate = (dateObject) => {
     const date = new Date(dateObject).toLocaleString('default', { month: 'long', year: 'numeric' });
@@ -45,7 +49,7 @@ const List = () => {
       </div>
 
       <div className="list-items">
-        {userItems.map((userItem) => (
+        {itemsFiltered.map((userItem) => (
           <div className="item" key={userItem.id}>
             <div className="item-date">{convertDate(userItem.item_added_at)}</div>
             {userItem.items.map((item) => (
