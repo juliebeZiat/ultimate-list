7550b7912d45189f9400ea1283aed83d28c095e3
VivienGrz <grzelka_vivien@hotmail.fr>
2022-04-11 03:03:45 +0200

    [integration] Page404

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -1,8 +1,16 @@
 // == Import style
 import './error404.scss';
+import { Link } from 'react-router-dom';
 
 const Error404 = () => (
-  <div>WOUPS la page demandée n'existe pas bichon</div>
+  <div className="notfound">
+    <div className="notfound-page">
+      <h1 className="notfound-page-message">WOUPS la page demandée n'existe pas bichon !</h1>
+      <p className="notfound-page-home"><Link to="/">revenir à la page d'accueil</Link></p>
+    </div>
+
+    <div className="notfound-gif"><iframe src="https://giphy.com/embed/vnFB72LzHtO4o" width="480" height="316" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+  </div>
 );
 
 // == Export
