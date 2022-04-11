85e1483b04eec277f3ab94453d04c4eceaff1b46
VivienGrz <grzelka_vivien@hotmail.fr>
2022-04-11 09:40:15 +0200

    [Integration] page404

--- src/components/Error404/index.js
+++ src/components/Error404/index.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const Error404 = () => (
   <div className="notfound">
     <div className="notfound-page">
-      <h1 className="notfound-page-message">WOUPS la page demandée n'existe pas bichon !</h1>
+      <h1 className="notfound-page-message">WOUPS la page demandée n'existe pas !</h1>
       <p className="notfound-page-home"><Link to="/">revenir à la page d'accueil</Link></p>
     </div>
 
-    <div className="notfound-gif"><iframe src="https://giphy.com/embed/vnFB72LzHtO4o" width="480" height="316" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+    <div className="notfound-gif"><iframe title="travolta" src="https://giphy.com/embed/vnFB72LzHtO4o" width="480" height="316" frameBorder="0" className="giphy-embed" allowFullScreen /></div>
   </div>
 );
 
