3439e3adb903a6e64c4693579c91e8bf6ba01464
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-11 09:28:55 +0200

    [Style] small changes

--- src/functions/items.js
+++ src/functions/items.js
@@ -4,7 +4,7 @@
  *  create an array of items where slug correspond to item.mode
  * @param {Array} itemList - every items
  * @param {string} searchedMode - mode's slug
- * @return {Object} - item found
+ * @return {Object} - items found
  */
 export function findItemsByMode(itemList, searchedMode) {
   const items = itemList.filter((item) => item.mode.name === searchedMode);
