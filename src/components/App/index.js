3468f39bebd219c8045d3ef710c63611f7d08b1c
LouiseLaroche <llarocheprigent@gmail.com>
2022-04-12 17:00:02 +0200

    [Style] small changes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -45,6 +45,11 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/connexion" element={<Login />} />
           <Route path="/inscription" element={<SignUp />} />
+
+          {/* delete those lines when token is store in a cookie */}
+          <Route path="/:slug/liste" element={<List />} />
+          <Route path="/:slug/ajouter" element={<Add />} />
+
           {logged && <Route path="/:slug/liste" element={<List />} />}
           {logged && <Route path="/:slug/ajouter" element={<Add />} />}
           {logged && <Route path="/*" element={<Error404 />} />}
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,8 +12,11 @@ const Home = () => {
       <h1 className="home-title">Ultimate List</h1>
       <h2 className="home-subtitle">Découvrez. Partagez. Créez.</h2>
       <p className="home-tagline">Créez d’incroyables todo-list pour sauvegarder et organiser tous vos souvenirs et recommandations de vos amis</p>
+      {/* uncommente when token is store in a cookie
       {!logged && <HomeLogout />}
       {logged && <HomeLogin />}
+      delete next line when token is store in a cookie */}
+      <HomeLogin />
     </div>
   );
 };
