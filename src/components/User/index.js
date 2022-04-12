94ff87c46637f0abb5c77ce89535aede318f6c68
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-12 17:49:34 +0200

    [Build][Fix] simplify code to display user username

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -7,16 +7,14 @@ import { useSelector } from 'react-redux';
 const User = () => {
   const isOpen = useSelector((state) => state.login.isSettingsOpen);
   const users = useSelector((state) => state.user.list);
-  const userName = useSelector((state) => state.login.username);
-  const userMacthList = users.filter((username) => username.username === userName);
+  const currentUser = useSelector((state) => state.login.username);
+  const user = users.find((username) => username.username === currentUser);
 
   return (
     <div className="user">
       {isOpen && (
       <div className="user-toggle">
-        {userMacthList.map((user) => (
-          <div className="user-toggle-hello" key={user.id}><h4>Coucou {user.username} </h4></div>
-        ))}
+        <div className="user-toggle-hello"><h4>Coucou {user.username} </h4></div>
         <div className="user-toggle-disconnect"><Link to="/">Se déconnecter</Link></div>
         <div className="user-toggle-delete"><Link to="/">Supprimer définitivement mon profil</Link></div>
       </div>
