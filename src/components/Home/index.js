3a6c83eae71834af8774845a8987ca314cc4b2ef
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-12 16:23:17 +0200

    [Fix] token to logged variable

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,15 +5,15 @@ import HomeLogin from './HomeLogin';
 import HomeLogout from './HomeLogout';
 
 const Home = () => {
-  const token = useSelector((state) => state.login.token);
+  const logged = useSelector((state) => state.login.logged);
 
   return (
     <div className="home">
       <h1 className="home-title">Ultimate List</h1>
       <h2 className="home-subtitle">Découvrez. Partagez. Créez.</h2>
       <p className="home-tagline">Créez d’incroyables todo-list pour sauvegarder et organiser tous vos souvenirs et recommandations de vos amis</p>
-      {token == null && <HomeLogout />}
-      {token !== null && <HomeLogin />}
+      {!logged && <HomeLogout />}
+      {logged && <HomeLogin />}
     </div>
   );
 };
