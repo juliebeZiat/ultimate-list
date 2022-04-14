01984859e84ac9f4b511cae371324d551ff3494c
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-14 15:35:31 +0200

    [Style] style error message after login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './login.scss';
 import { changeLoginField, loaderOn, logIn } from '../../actions/login';
 import Field from '../Field';
+import Error from '../../assets/icons/error.svg';
 
 const Login = () => {
   const usernameValue = useSelector((state) => state.login.username);
@@ -62,7 +63,12 @@ const Login = () => {
           />
         </div>
 
-        {loginFail && <div className="errorMessage">Mot de passe ou username incorrect</div>}
+        {loginFail && (
+          <div className="errorMessage-container">
+            <img src={Error} alt="" />
+            <div className="errorMessage">Nom d'utilisateur ou mot de passe incorrect. <br />Veuillez réessayer ou vous <Link className="errorMessage-link" to="/inscription">inscrire</Link>.</div>
+          </div>
+        )}
 
         <button
           className="connexion-form-submit"
