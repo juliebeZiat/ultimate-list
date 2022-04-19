c05ff2ea620788485b1cbecd2dccc40515f3441a
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-19 16:34:26 +0200

    [Style] 401 page + responsive

--- src/components/Errors/Error401/index.js
+++ src/components/Errors/Error401/index.js
@@ -1,11 +1,17 @@
 // == Import style
 import { Link } from 'react-router-dom';
+import ErrorGif from 'src/assets/404_domi.gif';
 
 const Error401 = () => (
   <div className="notfound">
     <div className="notfound-page">
-      <h1 className="notfound-page-message">Veuillez vous connecter</h1>
-      <p className="notfound-page-home"><Link to="/connexion">Se connecter</Link></p>
+      <h1 className="notfound-page-message">Oh bah ! Qu'est-ce que tu fais là ?</h1>
+      <p className="notfound-page-subtitle">Inscris-toi ou connecte-toi pour accéder au site</p>
+      <div className="notfound-buttons">
+        <Link to="/connexion"><button type="button">Connexion</button></Link>
+        <Link to="/inscription"><button type="button">Inscription</button></Link>
+      </div>
+      <img className="notfound-gif" src={ErrorGif} alt="" />
     </div>
   </div>
 );
