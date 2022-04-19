70d5ec8a0e83b28e4a9871628d8f13c825dc84c6
juliebeZiat <juliebeziat.m@gmail.com>
2022-04-19 09:49:18 +0200

    [Build] fetch items in reco page [Style] add style

--- src/components/Lists/Reco/index.js
+++ src/components/Lists/Reco/index.js
@@ -1,17 +1,30 @@
-// == Import
+// == Import style
 import './reco.scss';
-import { NavLink, useParams } from 'react-router-dom';
 import Toggle from 'src/assets/icons/toggle-off.svg';
+
+// == Import utils
+import { NavLink, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+// == Import functions
+import { findItemsByMode } from 'src/functions/items';
+
+// == Import actions
+import { sendItemToApi } from '../../../actions/userItems';
+
+// == Import components
 import Lists from '../index';
 
 // == Composant
 const Reco = () => {
+  const items = useSelector((state) => state.items.list);
   const { slug } = useParams();
+  const itemsFiltered = findItemsByMode(items, slug);
+  const dispatch = useDispatch();
 
   return (
     <div className="reco">
       <Lists />
-      Recommandations
       <div className="list-header-toggle">
         <NavLink
           to={`/${slug}/liste`}
@@ -35,6 +48,36 @@ const Reco = () => {
           Recommandations
         </NavLink>
       </div>
+
+      <ul className="add-reco-cards">
+        {itemsFiltered.slice(0, 8).map((item) => (
+          <li className="add-reco-cards-card" key={item.id}>
+            <img className="card-image" src={item.image} alt="" />
+            <div className="card-content">
+              <h3 className="card-title">{item.name}</h3>
+              <p className="card-description">{item.developer}{item.host}</p>
+              <div className="card-tags">
+                {item.tags.map((tag) => (
+                  <span
+                    className="card-tag"
+                    key={tag.id}
+                    style={{ backgroundColor: tag.color }}
+                  >
+                    {tag.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <button
+              className="card-button"
+              type="button"
+              aria-label="icon plus"
+              // On click on the icon plus, add the item selected to the userList
+              onClick={() => dispatch(sendItemToApi(item.id))}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
